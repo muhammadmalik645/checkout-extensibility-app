fc66fccc6d48a3b9c593465fc664c2278c4e0742
Muhammad Malik <muhammad.malik@ecomexperts.io>
2023-11-28 23:01:49 +0500

    first commit

---
+++ extensions/product-features/src/Checkout.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import {
+  Banner,
+  Text,
+  useTranslate,
+  reactExtension,
+  useTarget,
+  useAppMetafields,
+  useCartLineTarget,
+  useCartLines,
+  useApplyCartLinesChange,
+  useApi,
+  useSettings,
+
+} from '@shopify/ui-extensions-react/checkout';
+
+
+// const checkoutBlock = reactExtension('purchase.checkout.block.render',() => <Extension/>)
+// export { checkoutBlock }
+
+export default reactExtension('purchase.checkout.cart-line-item.render-after', ()=> <Extension/>)
+
+function Extension() {
+  const { query, i18n } = useApi();
+  const applyCartLinesChange = useApplyCartLinesChange();
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const merch = useCartLineTarget();
+  const productID = merch.merchandise.product.id
+  
+  useEffect(() => {
+    (async () => {
+      await fetchProducts(productID);
+      
+    })();
+  }, []);
+
+  async function fetchProducts(id) {
+    setLoading(true);
+    try {
+      const data = await query(
+        `{
+          product(id: "${id}") {
+            metafield(namespace:"custom",key: "banner") {
+              value
+              key
+            }
+            description
+          }
+        }`
+      );
+      console.log(data.data.product.metafield.value)      
+      setProducts(data.data.product.metafield.value);
+      
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  
+  const {title: merchantTitle, description: merchantDesc, collapsible: collapsibleStatus, status: merchantStatus} = useSettings();
+
+  const status = merchantStatus ?? 'info';
+  const titleBanner = merchantTitle ?? 'Custom Banner';
+  const description = merchantDesc ?? 'This is the description';
+  const collapsible= collapsibleStatus ?? true
+  let productMetafield = products ?? "empty"
+
+  return (
+    <Banner title={titleBanner} status={status} collapsible={collapsible}>
+      {productMetafield}
+    </Banner>
+  );
+}
